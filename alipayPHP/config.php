bf613b58bf564a9ec24240b8e314b66315978610
Acer-smk <1193136099@qq.com>
2019-03-28 15:23:17 +0800

    2019年3月20日09:23:19

---
+++ alipayPHP/config.php
@@ -0,0 +1,52 @@
+<?php
+$config = array(
+    //应用ID,您的APPID。
+    'app_id' => "2019031963591323",
+
+    //商户私钥，您的原始格式RSA私钥
+    'merchant_private_key' => "MIIEpAIBAAKCAQEAmmzsLflneggcCrJcFMg8zPbRg4DIu59smEl2nqS/ARbsGpWm
+mlpfhgIIi004rWwsqmCyeD4wKI6AA2uYPV5yIsm5EEEMP7we9se3y5K7wMbjAa2t
+pZNUofbmLDAy9oF80MHO50sozLcscfaokQwCJZVZFUD9WeRQenSYA76M8fT9Y4HR
+ywyH5r3DeLMEKtJoZhC1uddEENg6s740EV8YEVmliTzef6ACL4zsLqNUHhwFDBJe
+JNSuMBXZHveqwOjlUSWdywu/8KOe43OTDCmJkgfgbjEINYL9xObp7RovT7MwLin6
+ugXVB0SiM7BQhEAalf16KWJc2E0FuMLgNZPLCQIDAQABAoIBAEKuY/GaL0dvdwRb
+bg1/RVnP/jpFgugZeB/uIO1yABQtXLhCBJXEDllkSWI1bxLbkhHj/UyUteDdX4Oa
+kzt0q94/sszE+dAPoGM5bDDYZIBioSVZZJEJpFLntQ5Bpc/xq/7MoVYYiz6Snft7
+4d/4CnlGGxhlXsDRKLsn23hKcLwNPQh9qvGc830Hb5LBWxAD7fMxvumz+uLOkNjY
+QvwblNKu6CN7qPFF2SzRDOhlBn3IE0ZbCglo6lydabCF4RWZvwZqTuHsPyaJjuRU
+RkMVRXHXZ5Vqw4isfjj64+t8pAs1gqh8wViWmHjVI8F5/2pnXjbdjaX+oE1lHcJ8
+2NeTPC0CgYEAzdUOWN/MzWZL1/B4Bb+gJHg0Kp3q80DNhqORbkRlkPVPlHx+tBgK
+KsojqcrjAkeWiJFnUW587E8LLBadV57igj5Zy9Ra53DpYgzUNStuksBR7rFcnJrR
+346kWLe+nM6cePqrVQAy8DsrCczD7mSwap3koWlQ/tgnjel+1xm5wGMCgYEAwBBU
+s7wzR6sywz9YbEXqs2MO+a1qPkdgPSV8pMKqeSFzkM54YQR590BD5ElWN5i2WDV2
+InJFKQmrpGmBJVF9//9TW4coiTqCF7tNI9MlpqHUiz7CDD5khji15hqoObdzo9VI
+hB2tSbRSJa5PQoVlnd/fj6bMlfg1gXA9B+I8RKMCgYEAgbhvtVU+hoic7fewPLc7
+HyDeh6lExI+nt93oLt1OQWBJCiS32Zp49eS1OzIRhYNGfMVenvSKKIfcW6nY+bq5
+uCnBf/NSYyBHBbNeHjBEHJq2SD9hRZVRBZqpOvx6VerktF4OkqBwefRgOGnjZZ/x
+iw/9Yji0ghgKPabVeIzPA08CgYEArsgtzMLgB8CMGZJz7VRTGZxz7FMb9EEQG5UZ
+sPZWHJWMjaLXebKRK6iYIA99HmsixXMhZoeG2KmsQZqhpExc1bKNMSX2hatw6BfT
+Q17LTLDIs08RWAMPh/Xj0ts/bAa+fcGClHhNUl1+xDu0dVzf9KPe1uN8Gp4eUwTJ
+BXukTPECgYBiB3t8EFm33djGzFmOdLJAGeqtVE9qza4xmDOyY8C4Q9O2yyfSzCiM
+cKop3jabBE4WioLiUPj9aYZdemxBNJ8DwIpOtpgY4CI5KxdqDtDkRXUJhLuqyVws
+2uOzvWy4AKLOUwwXOwTmRJivcHE58wKKKgE6MDK7/tFGEA1c+PjlkA==",
+
+    //异步通知地址  后台返回
+    'notify_url' => "http://lcke.top/alipay.trade.wap.pay-PHP-UTF-8/notify_url.php",
+
+    //同步跳转     页面返回支付结果
+    'return_url' => "http://lcke.top",//"http://mitsein.com/alipay.trade.wap.pay-PHP-UTF-8/return_url.php",
+
+    //编码格式
+    'charset' => "UTF-8",
+
+    //签名方式
+    'sign_type' => "RSA2",
+
+    //支付宝网关
+    'gatewayUrl' => "https://openapi.alipay.com/gateway.do",
+
+    //支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+    'alipay_public_key' => "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl/gG7TqijKSg3prMBSIcSsS/aqTe4zxgP+VvbnUBZXL0WaSGpsuJ7ffXl3XKweNz+L8qAX7bwfU6+1pjSrzhX0HGCeYhoP+ScHniu+P0eHGNRWqHHJ0LcJ9B+oRfAMFBx6gqMaCGT59dCnXqxXZlMNGrA4EL1RbzUtnqpT2S043rJIk3/ZBq9XatQOCabuHAWG/TM1MxhxyiBPWRAZhbeDhQWfTkV+MkJUPt+A7cEvPSYB343yi78GPtbIobkTrzGElHbv7BSrBzTSTjqk9Fun4bMuOK5kE7R4iQSo/l1E5O8iur+CLKuiR7Ml2Kfr8Veh7AacjBvBBdka925x5HFQIDAQAB",
+
+
+);
--- js/show.js
+++ js/show.js
@@ -1,36 +1,77 @@
-
-
+var allmoney;
 
 //购买商品数量减一，剩余商品数目加一
-function subplus(temp1) {
-    let multmp = $("#item-1")[0].innerText;
-    let multmpleft = $("#item-1-left")[0].innerText;
+function subplus(temp1, num) {
+    allmoney = $("#allMoney")[0].innerText;
+    let multmp = $(".inputsm")[num].innerText;
+    let multmpleft = $(".item-left")[num].innerText;
+    let permoney = $('.item-1-left')[num].innerText;
+    let quantity = $('#allQuantity')[0].innerText;
+
     if (temp1 == "sub") {
         if (parseInt(multmp) > 0) {
-            $("#item-1")[0].innerText = parseInt(multmp) - 1;
-            //console.log(multmp);
-            $("#item-1-left")[0].innerText = parseInt(multmpleft) + 1;
+            $(".inputsm")[num].innerText = parseInt(multmp) - 1;
+            $(".item-left")[num].innerText = parseInt(multmpleft) + 1;
+            $("#allMoney")[0].innerText = (parseFloat(allmoney) - parseFloat(permoney)).toFixed(2);
+            $('#allQuantity')[0].innerText = parseInt(quantity) - 1;
         }
     }
     else if (temp1 == "plus") {
         if (parseInt(multmpleft) > 0) {
-            $("#item-1")[0].innerText = parseInt(multmp) + 1;
-            //console.log(multmp);
-            $("#item-1-left")[0].innerText = parseInt(multmpleft) - 1;
+            $(".inputsm")[num].innerText = parseInt(multmp) + 1;
+            $(".item-left")[num].innerText = parseInt(multmpleft) - 1;
+            $("#allMoney")[0].innerText = (parseFloat(allmoney) + parseFloat(permoney)).toFixed(2);
+            $('#allQuantity')[0].innerText = parseInt(quantity) + 1;
         }
     }
+    //购物车图标上下相同
+    $('.balltxt')[0].innerText = parseInt($('#allQuantity')[0].innerText);
 }
-function f() {
-    
+
+
+function doSubmitForm() {
+    var form = document.getElementById('test-form');
+    let str = $('.goodsitem');
+    let num = $('.inputsm');
+    let detail=[];
+    for (let i = 0; i < str.length; i++) {
+        detail[i] = str[i].innerText + "," + num[i].innerText;
+    }
+    console.log(detail);
+    $('#allpic')[0].value = $("#allMoney")[0].innerText;
+    $('#detail')[0].value = detail;
+    form.submit();
 }
-//页面跳转
+
+/*
+function showPay() {
+    let str = $('.goodsitem');
+    let num = $('.inputsm');
+    let detail;
+    for (let i = 0; i < str.length; i++) {
+        detail = str[i].innerText + "," + num[i].innerText;
+    }
+    var jqxhr = $.post('../alipayPHP/index.php', {
+        allpic: allmoney,
+        detail: detail,
+    }).done(function (data) {
+        console.log("success");
+    }).fail(function (xhr, status) {
+        console.log('fail');
+    }).always(function () {
+        window.open('../alipayPHP/index.php')
+    });
+
+}
+
+
 function test(str) {
     // window.confirm("你点击了申请") ;
     if (str == "pay") {
         window.open("pay.html", target = "_parent");
     }
-
     else if (str == "request") {
         window.open("request.html", target = "_parent");
     }
 }
+*/
